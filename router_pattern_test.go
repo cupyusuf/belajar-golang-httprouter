9e92cf134f82cf1b4352b494246d3248e892281d
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-04 12:49:35 +0700

    feat: add named parameter

---
+++ router_pattern_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRouterPattrnNamedParameter(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/products/:id/items/:itemId", func(writer http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		id := params.ByName("id")
+		itemId := params.ByName("itemId")
+		text := "Product " + id + " Item " + itemId
+		fmt.Fprint(writer, text)
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/products/1/items/1", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Product 1 Item 1", string(body))
+}
